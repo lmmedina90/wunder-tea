0fcfee01af03152833cc43de99ba47a1fefe0b54
Louis Medina <viewsatwork@gmail.com>
2015-09-28 11:44:28 -0400

    User Fabricator + User Model Test

---
+++ test/models/user_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+require 'minitest/autorun'
+
+class UserTest < ActiveSupport::TestCase
+
+  describe "validations" do
+    let(:user) { Fabricate(:user) }
+
+    it "has a valid fabricator" do
+      Fabricate.build(:user).wont_be_nil
+    end
+  end
+end
