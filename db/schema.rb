d0513aa549b6ae2587c1549aec7808f3d16e3923
Louis Medina <viewsatwork@gmail.com>
2015-09-28 12:16:17 -0400

    Add Teas Table to Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150923163751) do
+ActiveRecord::Schema.define(version: 20150928160838) do
+
+  create_table "teas", force: :cascade do |t|
+    t.string   "type",       limit: 255
+    t.integer  "quantity",   limit: 4
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name", limit: 255
